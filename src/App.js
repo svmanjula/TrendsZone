fe070c244ca9eadce9378673602ce039b974399c
Manjula <ssmanjulasv91@gmail.com>
2022-12-13 21:40:31 +0530

    updated the homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Header from './Components/Header';
-import Sidebar from './Components/Sidebar';
+
+import "./App.css";
+
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import { Routes, Route } from "react-router-dom";
+import Loginpage from "./pages/LoginPage";
+import Signuppage from "./pages/SignupPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import Wishlistpage from "./pages/WishlistPage";
+import MainRouter from "./routes/Router";
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Sidebar/>
+      <MainRouter/>
     </div>
   );
 }
---
+++ src/Pages/ProductPage.js
@@ -0,0 +1,124 @@
+import React, { useContext } from "react";
+import Header from "../components/Header/Header";
+import Sidebar from "../components/Sidebar/Sidebar";
+import "./ProductPage.css";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { CartContext } from "../context/Context";
+import Rating from "../components/Rating";
+
+const ProductPage = () => {
+  const {
+    products,
+    filterState: { sort, byRating, bySearch, byCategory },
+    state: { cart, wishlist },
+
+    dispatch,
+  } = useContext(CartContext);
+
+  const filterProducts = () => {
+    let sortedProducts = [...products];
+
+    if (sort) {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        sort === "lowToHigh" ? a.price - b.price : b.price - a.price
+      );
+    }
+    if (bySearch) {
+      sortedProducts = sortedProducts.filter((prod) =>
+        prod.title.toLowerCase().includes(bySearch)
+      );
+    }
+    if (byCategory) {
+      sortedProducts = sortedProducts.filter(
+        (prod) =>  byCategory.includes(prod.category)
+      );
+    }
+    
+
+   
+    if (byRating) {
+      sortedProducts = sortedProducts.filter((prod) => {
+        console.log("rating", prod.rating.rate >= byRating);
+        return prod.rating.rate >= byRating;
+      });
+    }
+
+     else {
+      return sortedProducts;
+    }
+  };
+  console.log(byCategory);
+
+  return (
+    <div>
+      <Header />
+      <div className="products-container">
+        {filterProducts()?.map((product) => (
+          <div key={product.id} className="product-container ">
+            <img alt="img" src={product.image} className="prod-img" />
+
+            {wishlist.some((prod) => prod.id == product.id) ? (
+              <AiFillHeart
+                onClick={() => {
+                  dispatch({
+                    type: "REMOVE_FROM_WISHLIST",
+                    payload: product,
+                  });
+                }}
+                className="wishlist-icon filled-icon"
+              />
+            ) : (
+              <AiOutlineHeart
+                onClick={() => {
+                  dispatch({
+                    type: "ADD_TO_WISHLIST",
+                    payload: product,
+                  });
+                }}
+                className="wishlist-icon"
+              />
+            )}
+
+            <div className="prod-title">
+              {product.title.split(" ").slice(0, 3).join(" ")}
+            </div>
+            {/* <div className="prod-description">
+            {product.description}
+          </div> */}
+            <div className="prod-cost">${product.price} </div>
+            <Rating rating={product.rating.rate} />
+            {cart.some((prod) => prod.id === product.id) ? (
+              <button
+                className="prod-button remove-button"
+                onClick={() => {
+                  dispatch({
+                    type: "REMOVE_FROM_CART",
+                    payload: product,
+                  });
+                }}
+              >
+                Remove from cart
+              </button>
+            ) : (
+              <button
+                className="prod-button"
+                onClick={() => {
+                  dispatch({
+                    type: "ADD_TO_CART",
+                    payload: product,
+                  });
+                }}
+              >
+                Add to cart
+              </button>
+            )}
+          </div>
+        ))}
+      </div>
+
+      <Sidebar />
+    </div>
+  );
+};
+
+export default ProductPage;
